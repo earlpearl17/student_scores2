5bb3c7ce67df8056fce6bc55d4d32546596770d3
unknown <earlpearl17@gmail.com>
2022-02-22 04:15:56 -0500

    Pre Nginx & Gunicorn

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
         'NAME': 'studentdb2',
         'USER': 'studentdbuser',
         'PASSWORD': 'django1234',
+        #'HOST': '127.0.0.1',
         'HOST': 'db',
         'PORT': '5432',
     }
